a1dfb5f9486d529540652b426e12fccaca0d4709
Grayden Hormes <graydenhormes@gmail.com>
2023-07-07 21:47:40 -0700

    a plane

--- Assets/Scripts/ProceduralMesh/Generators/SquareGrid.cs
+++ Assets/Scripts/ProceduralMesh/Generators/SquareGrid.cs
@@ -12,37 +12,37 @@ namespace ProceduralMeshes.Generators {
 
 		public int JobLength => Resolution * Resolution;
 
-		public Bounds Bounds => new Bounds(new Vector3(0.5f, 0.5f), new Vector3(1f, 1f));
+		public Bounds Bounds => new Bounds(Vector3.zero, new Vector3(1f, 0f, 1f));
 		
 		public int Resolution { get; set; }
 
 		public void Execute<S> (int i, S streams) where S : struct, IMeshStreams {
 			int vi = 4 * i, ti = 2 * i;
 			
-			int y = i / Resolution;
-			int x = i - Resolution * y;
+			int z = i / Resolution;
+			int x = i - Resolution * z;
 			
-			var coordinates = float4(x, x + 0.9f, y, y + 0.9f);
+			var coordinates = float4(x, x + 1f, z, z + 1f) / Resolution - 0.5f;
 
-			var vertex = new Vertex();
-			vertex.normal.z = -1f;
+			var vertex = new Vertex();			
+			vertex.normal.y = 1f;
 			vertex.tangent.xw = float2(1f, -1f);
 			
-			vertex.position.xy = coordinates.xz;
+			vertex.position.xz = coordinates.xz;
 			streams.SetVertex(vi + 0, vertex);
 			
-			vertex.position.xy = coordinates.yz;
+			vertex.position.xz = coordinates.yz;
 			vertex.texCoord0 = float2(1f, 0f);
 			streams.SetVertex(vi + 1, vertex);
 
-			vertex.position.xy = coordinates.xw;
+			vertex.position.xz = coordinates.xw;
 			vertex.texCoord0 = float2(0f, 1f);
 			streams.SetVertex(vi + 2, vertex);
 
-			vertex.position.xy = coordinates.yw;
+			vertex.position.xz = coordinates.yw;
 			vertex.texCoord0 = 1f;
 			streams.SetVertex(vi + 3, vertex);
-			
+
 			streams.SetTriangle(ti + 0, vi + int3(0, 2, 1));
 			streams.SetTriangle(ti + 1, vi + int3(1, 2, 3));
 		}
