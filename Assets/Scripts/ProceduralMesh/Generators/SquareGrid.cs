c0c767a70e7dd280b92abd535ba337526933d211
Grayden Hormes <graydenhormes@gmail.com>
2023-07-07 21:41:13 -0700

    generating all quads

--- Assets/Scripts/ProceduralMesh/Generators/SquareGrid.cs
+++ Assets/Scripts/ProceduralMesh/Generators/SquareGrid.cs
@@ -17,26 +17,34 @@ namespace ProceduralMeshes.Generators {
 		public int Resolution { get; set; }
 
 		public void Execute<S> (int i, S streams) where S : struct, IMeshStreams {
+			int vi = 4 * i, ti = 2 * i;
+			
+			int y = i / Resolution;
+			int x = i - Resolution * y;
+			
+			var coordinates = float4(x, x + 0.9f, y, y + 0.9f);
+
 			var vertex = new Vertex();
 			vertex.normal.z = -1f;
 			vertex.tangent.xw = float2(1f, -1f);
 			
-			streams.SetVertex(0, vertex);
+			vertex.position.xy = coordinates.xz;
+			streams.SetVertex(vi + 0, vertex);
 			
-			vertex.position = right();
+			vertex.position.xy = coordinates.yz;
 			vertex.texCoord0 = float2(1f, 0f);
-			streams.SetVertex(1, vertex);
+			streams.SetVertex(vi + 1, vertex);
 
-			vertex.position = up();
+			vertex.position.xy = coordinates.xw;
 			vertex.texCoord0 = float2(0f, 1f);
-			streams.SetVertex(2, vertex);
+			streams.SetVertex(vi + 2, vertex);
 
-			vertex.position = float3(1f, 1f, 0f);
+			vertex.position.xy = coordinates.yw;
 			vertex.texCoord0 = 1f;
-			streams.SetVertex(3, vertex);
+			streams.SetVertex(vi + 3, vertex);
 			
-			streams.SetTriangle(0, int3(0, 2, 1));
-			streams.SetTriangle(1, int3(1, 2, 3));
+			streams.SetTriangle(ti + 0, vi + int3(0, 2, 1));
+			streams.SetTriangle(ti + 1, vi + int3(1, 2, 3));
 		}
 	}
 }
